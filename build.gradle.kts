02f9f82f3e8cfc788cf8d3a0f55651953da978a8
mzakidalski <mzakidalski@gmail.com>
2025-05-25 20:09:01 +0200

    Next modules added

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+
     testImplementation("org.hamcrest:hamcrest-library:3.0")
 
     testImplementation("org.mockito:mockito-core:5.18.0")
