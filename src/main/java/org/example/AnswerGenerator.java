886ef7fce5453ac67be9d3b8d849a9b5fdd8aa19
mzakidalski <mzakidalski@gmail.com>
2025-05-25 20:37:03 +0200

    Next modules added

--- src/main/java/org/example/AnswerGenerator.java
+++ src/main/java/org/example/AnswerGenerator.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.List;
+
 public class AnswerGenerator {
     public String generateAnswer() {
         return "42";
+    }
+
+    public List<String> generateListAnswer() {
+        return List.of("42", "is", "the", "answer");
     }
 }
--- src/test/java/org/example/AnswerGeneratorTest.java
+++ src/test/java/org/example/AnswerGeneratorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.*;
 
@@ -15,5 +15,17 @@ class AnswerGeneratorTest {
     void generateAnswer() {
         var answerGenerator = new AnswerGenerator();
         assertThat(answerGenerator.generateAnswer(), equalTo("42"));
+        assertThat(answerGenerator.generateAnswer(), startsWith("4"));
+        assertThat(answerGenerator.generateAnswer(), endsWith("2"));
     }
+
+    @Test
+    void generateAnswerAsList() {
+        var answerGenerator = new AnswerGenerator();
+        var answer = answerGenerator.generateListAnswer();
+        assertThat(answer, hasSize(4));
+        assertThat(answer, contains("42", "is", "the", "answer"));
+    }
+
+
 }
