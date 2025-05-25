6ab21e945593422f13e92ec4f7022cdaf3b945ee
mzakidalski <mzakidalski@gmail.com>
2025-05-25 21:14:30 +0200

    other testing preparation

---
+++ src/main/java/org/example/MyEnum.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum MyEnum {
+    ALPHA, BETA, GAMMA
+}
--- src/test/java/org/example/AnswerGeneratorTest.java
+++ src/test/java/org/example/AnswerGeneratorTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -45,6 +47,19 @@ class AnswerGeneratorTest {
     void generateAnswerThrowingException() {
         var answerGenerator = new AnswerGenerator();
         Exception ex = assertThrows(IllegalArgumentException.class, () -> answerGenerator.generateAnswerThrowingAssertions(42));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "ALPHA, 1.2, 'aa'",
+            "BETA, 1.3, 'ab'"
+    })
+    @DisplayName("parametrized test")
+    void testMultipleParameters(MyEnum myEnum, float value, String result) {
+        var answerGenerator = new AnswerGenerator();
+        assertThat(myEnum, is(not(MyEnum.GAMMA)));
+        assertThat(value, is(greaterThan(1.0f)));
+        assertThat(result, startsWith("a"));
     }
 
 
